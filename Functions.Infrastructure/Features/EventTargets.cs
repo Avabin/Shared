b1115ba53c56afd0e720098bb7fbb34e09823ccc
Andrzej Piotrowski <andrzej.piotrowski76@gmail.com>
2022-04-02 00:11:00 +0200

    Add Players and Sesssions

--- Functions.Infrastructure/Features/EventTargets.cs
+++ Functions.Infrastructure/Features/EventTargets.cs
@@ -2,6 +2,16 @@
 
 public static class EventTargets
 {
+    // characters
     public static readonly string CharactersApi = "characters-api";
     public static readonly string CharactersDb = "characters-persistence";
+    
+    // players
+    public static readonly string PlayersApi = "players-api";
+    public static readonly string PlayersDb = "players-persistence";
+    
+    // sessions
+    public static readonly string SessionsApi = "sessions-api";
+    public static readonly string SessionsDb = "sessions-persistence";
+    
 }
